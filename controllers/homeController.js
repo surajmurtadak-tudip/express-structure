5e9a08c863e15064f42fcbd8225d73c7aaaf661d
surajmurtadak-tudip <suraj.murtadak@tudip.com>
2023-09-11 16:27:01 +0530

    render html page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,9 @@
+
+const path = require("path");
+
 const homeController = (req,res)=>{
-    res.send("Welcome to express MVC structure.");
+    // res.send("Welcome to express MVC structure.");
+    res.sendFile(path.join(__dirname,"../public","/home.html"));
 };
 
 module.exports = homeController;
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const router = require("./routes/homeRoute");
 
-
+app.use(express.static("public"));
 app.use("/",router);
 app.listen(4000,()=>console.log("server up and running"));
